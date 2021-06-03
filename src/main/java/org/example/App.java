73df6d94188cd0735cd91e157e1b77ff87659592
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-03 15:37:51 -0400

    Final solution w/ Added comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,16 @@
 package org.example;
 import java.util.Scanner;
-
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Anisha Ranjan
  */
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.printf("What is the quote? ");
+        System.out.print("What is the quote? ");
         Scanner scan = new Scanner(System.in);
         String quote = scan.nextLine();
-        System.out.printf("Who said it? ");
+        System.out.print("Who said it? ");
         String author = scan.nextLine();
 
         System.out.printf(author + " says, \"%s\" ", quote);
